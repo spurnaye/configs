dd2ff22324486b20fabd1340caa3706952a8e36f
Saurabh Purnaye <spurnaye@users.noreply.github.com>
2015-03-20 10:58:52 -0400

    Update git_config.sh

--- git_config.sh
+++ git_config.sh
@@ -1,4 +1,4 @@
-# bash << (curl -s https://github.com/spurnaye/configs/raw/master/git_config.sh)
+# bash < <(curl -s https://raw.githubusercontent.com/spurnaye/configs/master/git_config.sh)
 git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.ci commit
